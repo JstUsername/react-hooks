b0ce5a8382acf1aecc4bcac54bed7dc7c002df28
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2026-01-20 16:25:15 +0300

    fix: browser router заменен на hash, для совместимости с gh pages

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import { DocumentVisibilityHookPage } from './pages/document-visibility-hook-page.tsx';
 import { MediaQueryHookPage } from './pages/media-query-hook-page.tsx';
 import { MediaQueryComponentPage } from './pages/media-query-component-page.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route element={<App />} index />
         <Route path="/document-visibility-hook" element={<DocumentVisibilityHookPage />} />
         <Route path="/media-query-hook" element={<MediaQueryHookPage />} />
         <Route path="/media-query-component" element={<MediaQueryComponentPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
